3f39a8bd16e10e2545b750b64737c71c4be9ba96
kjm3757 <kjm03070519@gmail.com>
2025-05-08 17:50:37 +0900

    Feat: data 파일 다운 기능 추가

--- manito_app/app.py
+++ manito_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, send_file
 import json, os, random
 
 app = Flask(__name__)
@@ -169,6 +169,15 @@ def reset():
     }
     save_data(empty_data)
     return redirect(url_for('admin'))
+
+@app.route('/download_data')
+def download_data():
+    return send_file(
+        'data.json',
+        mimetype='application/json',
+        as_attachment=True,
+        download_name='data.json'
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
